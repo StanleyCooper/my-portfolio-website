bce644f7662cf7aa2232445ad1737960db219a91
Stanley Cooper <scooperlu@gmail.com>
2022-02-19 09:43:16 -0500

    Added Navagation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>
+    <Fragment>
       <Navbar />
-      <Home />
-    </div>
+      <Switch>
+        <Route exact path = "/">
+          <Home />
+        </Route>
+        <Route path = "/Projects">
+          <Projects />
+        </Route>
+        <Route path = "/Resume">
+          <Resume />
+        </Route>
+        <Route path = "/Contact">
+          <Contact />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Fragment>
   );
 }
 
