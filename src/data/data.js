b2c02abf70ba32fd3135379e6bfc0c7242a0dd10
Stanley Cooper <scooperlu@gmail.com>
2022-05-19 16:14:30 -0400

    Fixed data for current projects

--- src/data/data.js
+++ src/data/data.js
@@ -6,67 +6,34 @@ export const hero = {
 
 export const projects = [
     {
-        name: "Project Tracker",
-        description: "Track, modify, and help speed up the process of projects with this project tracking tool",
-        image: "https://training.npr.org/wp-content/uploads/2016/06/dreamstime_l_260891731.jpg",
-        technology: "React.js | Node.js | TailwindCSS",
-        github: "https://github.com/StanleyCooper/estimating-app"
-    }
+        name: "WeLoveMovies",
+        description: "Movie database and information website with data on movie times, showings, and reviews",
+        image: "./photos/we-love-movies.png",
+        technology: "React.js | Node.js | Express.js | PostgreSQL | HTML | CSS",
+        github: "https://github.com/StanleyCooper/WeLoveMovies",
+        deploy: "https://welovemovies-front-side.herokuapp.com/",
+    },
+    {
+        name: "GrubDash",
+        description: "Mock web application for ordering food online",
+        image: "./photos/grub-dash.png",
+        technology: "React.js | Node.js | Express.js | HTML | CSS",
+        github: "https://github.com/StanleyCooper/GrubDash",
+        deploy: "https://grubdash-front-side.herokuapp.com/",
+    },
+    {
+        name: "Flashcards",
+        description: "Flashcard web application for creating decks of cards to help with studying.",
+        image: "./photos/flashcards.png",
+        technology: "React.js | Node.js | HTML | CSS",
+        github: "https://github.com/StanleyCooper/Flashcard-o-matic",
+        deploy: "https://flash-o-matic.herokuapp.com/",
+    },
 ]
 
 export const blogPosts = [
     {
         name: "A Blog Post #1",
-        description: "A cool blogpost about cool stuff!",
-        url: "lol",
-    },
-    {
-        name: "A Blog Post #2",
-        description: "A cool blogpost about cool stuff!",
-        url: "lol",
-    },
-    {
-        name: "A Blog Post #2",
-        description: "A cool blogpost about cool stuff!",
-        url: "lol",
-    },
-    {
-        name: "A Blog Post #2",
-        description: "A cool blogpost about cool stuff!",
-        url: "lol",
-    },
-    {
-        name: "A Blog Post #2",
-        description: "A cool blogpost about cool stuff!",
-        url: "lol",
-    },
-    {
-        name: "A Blog Post #2",
-        description: "A cool blogpost about cool stuff!",
-        url: "lol",
-    },
-    {
-        name: "A Blog Post #2",
-        description: "A cool blogpost about cool stuff!",
-        url: "lol",
-    },
-    {
-        name: "A Blog Post #2",
-        description: "A cool blogpost about cool stuff!",
-        url: "lol",
-    },
-    {
-        name: "A Blog Post #2",
-        description: "A cool blogpost about cool stuff!",
-        url: "lol",
-    },
-    {
-        name: "A Blog Post #2",
-        description: "A cool blogpost about cool stuff!",
-        url: "lol",
-    },
-    {
-        name: "A Blog Post #2",
         description: "A cool blogpost about cool stuff!",
         url: "lol",
     },
